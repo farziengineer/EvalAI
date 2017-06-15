d58d595ebe5706be86024e0f8369cae57b57f67c
Deshraj <deshrajdry@gmail.com>
2017-06-16 03:10:31 +0530

    change in flow

--- apps/hosts/utils.py
+++ apps/hosts/utils.py
@@ -4,3 +4,9 @@ from .models import ChallengeHost
 def get_challenge_host_teams_for_user(user):
     """Returns challenge host team ids for a particular user"""
     return ChallengeHost.objects.filter(user=user).values_list('team_name', flat=True)
+
+
+def is_user_a_host_of_challenge(user, challenge_pk):
+    """Returns boolean if the Participant Team participated in particular Challenge"""
+    challenge_host_teams = get_challenge_host_teams_for_user(user)
+    return Challenge.objects.filter(pk=challenge_pk, challenge_host__in=challenge_host_teams).exists()
