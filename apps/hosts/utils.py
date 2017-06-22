4eb3700fa3febd90febf82f6cdc5525cc06c5562
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-06-23 01:06:08 +0530

    Fix Review Changes

--- apps/hosts/utils.py
+++ apps/hosts/utils.py
@@ -8,6 +8,6 @@ def get_challenge_host_teams_for_user(user):
 
 
 def is_user_a_host_of_challenge(user, challenge_pk):
-    """Returns boolean if the Participant Team participated in particular Challenge"""
+    """Returns boolean if the Participant participated in particular Challenge"""
     challenge_host_teams = get_challenge_host_teams_for_user(user)
     return Challenge.objects.filter(pk=challenge_pk, creator_id__in=challenge_host_teams).exists()
